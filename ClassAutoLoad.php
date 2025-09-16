9443c95cd963ba2059880eb8ece8bce40ba11dab
Daniwanjohi <daniella.wanjohi@strathmore.edu>
2025-09-16 20:47:54 +0300

    Intorduced Class Auto Load

---
+++ ClassAutoLoad.php
@@ -0,0 +1,17 @@
+<?php
+$directory = [
+    'Layouts',
+    'Forms'
+];
+
+spl_autoload_register(function($className) use ($directory){
+    foreach ($directory as $dir){
+        $filepath = __DIR__ . '/' . $DIR . '/' . $className . '.php';  
+        if (file_exists($filePath)){
+            require_once $filePath;
+            return;
+        }  
+    }
+});
+
+// Create various instances to test autoloading
